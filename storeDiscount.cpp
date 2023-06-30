2f56a1d0d0d1f2b29e8a153460f32dd4b754a574
JSerr5 <92064444+JSerr5@users.noreply.github.com>
2023-06-30 09:31:11 -0500

    Add files via upload

---
+++ storeDiscount.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using  namespace std;
+
+int main(){
+
+    float productCost, finalPrice;
+
+    cout << "Welcome to the e-market" << endl;
+    cout << "Introduce the price of the Product: ";
+    cin >> productCost;
+
+    if (productCost < 50){   
+        cout << "No available discount: $" << productCost << endl;
+    }
+
+    else if (50 <= productCost && productCost < 100){    
+        finalPrice = productCost-(productCost*0.05);
+        cout << "You recived a 5% discount: $" << finalPrice << endl;
+    }
+
+    else{
+        finalPrice = productCost-(productCost*0.1);
+        cout << "You recived a 10% discount: $" << finalPrice << endl;
+    }
+
+    cout << "Press Enter to exit";
+    getchar();
+    return 0;
+}
